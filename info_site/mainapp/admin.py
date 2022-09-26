2da25b81700ba55eae20e39bead43fe301528563
vbasoon <vbasoon@gmail.com>
2022-09-26 17:19:49 +0300

    add templates_tags

--- info_site/mainapp/admin.py
+++ info_site/mainapp/admin.py
@@ -4,9 +4,11 @@ from .models import *
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'is_published')
+    list_display = ('id', 'title', 'photo', 'time_create', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_create')
 
 
 class CategoryAdmin(admin.ModelAdmin):
