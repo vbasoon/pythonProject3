4656ecb4f46e72d48260ff1e214a74b147797c3b
Vlad Basoon <vbasoon@gmail.com>
2022-10-14 11:54:08 +0300

    deploy app

--- info_site/info_site/settings.py
+++ info_site/info_site/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-import django heroku
+#import django_heroku
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-1fbeq#44u@%p$69)i@v%ljl8mle%h6z0m81i%bbbr9j@btmxf4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,4 +131,4 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
