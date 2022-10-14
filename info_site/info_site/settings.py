0091738d73a3719bfd199aabb84135f20d54bd4b
Vlad Basoon <vbasoon@gmail.com>
2022-10-14 12:05:44 +0300

    deploy app

--- info_site/info_site/settings.py
+++ info_site/info_site/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'uk'
 
 TIME_ZONE = 'Europe/Helsinki'
 
